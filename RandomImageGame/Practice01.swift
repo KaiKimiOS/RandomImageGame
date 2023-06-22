ad2615bc2ec53abd13fac997dcea3ecf23e5650c
Kai Kim <sdffs310@naver.com>
2023-06-22 23:37:35 +0900

    최종화면에서 디테일 수정
    마지막 화면에서 최종정리 및 초기화진행

--- RandomImageGame/Practice01.swift
+++ RandomImageGame/Practice01.swift
@@ -18,124 +18,129 @@ struct Practice01: View {
     //1. NavigationLink를 통해 이동가능하다 또한, .disable을 사용하여 true/false로 변경가능
     //2. Vstack안에서 if조건문을 사용해서 만약 참이면 앞에 만들어 놓은 VStack , False이면 뒤에 만들어놓은거 띄우기
     @State var onOff: Bool = true //지금은 2번째 방법 사용중
+    @State var onOff2: Bool = false
     
-
-
+    
+    
     var body: some View {
-       // NavigationView { // NavigationLink를 사용하기 위해서 NavigationView 사용
-            VStack{
-                if onOff != true {
-                    VStack {
-                        Spacer()
-                        Text("축하합니다").font(.largeTitle)
-                        Text("총 \(answer)개 문제를 맞히셨습니다!!").font(.largeTitle)
-                            .padding(.bottom, 10)
+        // NavigationView { // NavigationLink를 사용하기 위해서 NavigationView 사용
+        VStack{
+            if onOff != true {
+                VStack {
+                    Spacer()
+                    Text("축하합니다").font(.largeTitle)
+                    Text("\(answer)개 문제를 맞히셨습니다!!").font(.largeTitle)
+                        .padding(.bottom, 10)
+                    
+                    
+                  if onOff2 != true  { HStack{
+                        
                         Button {
                             nextLevel()
-                            
                         } label: {
                             Text("Next Level ➡️ 💪🏻").font(.largeTitle)
-                        }
-
-                        Spacer()
-                        
-                    }} else {
-                        Text("Round \(stage+1)").font(.largeTitle).foregroundColor(.cyan)
-                        
-                        // 이미지를 stage(Int)에 맞게 넣어주기
-                        Image(quiz[stage].imgName)
-                            .resizable() // 이미지가 화면에 fit하게 만듬
-                            .frame(width: 350, height: 300)
-                            .padding()
-                        VStack{
-                            HStack{
-                                VStack{
-                                    Text("1번")
-                                    Text("2번")
-                                    Text("3번")
-                                    Text("4번")
-                                    
-                                }
-                                .font(.largeTitle)
-                                VStack{
-                                    
-                                    Button {
-                                        buttonClick(0)
-                                    } label: {
-                                        //보기란을 stage(Int)에 맞게 넣어주기
-                                        Text(quiz[stage].answers[0]).frame(width:300)
-                                    }
-                                    .tag(0)// 정답 answerIndex와 tag를 일치시켜서 정답확인 가능하게 함
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
-                                    
-                                    Button {
-                                        
-                                        buttonClick(1)
-                                    } label: {
-                                        Text(quiz[stage].answers[1]).frame(width:300)
-                                    }
-                                    .tag(1)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
-                                    
-                                    Button {
-                                        
-                                        buttonClick(2)
-                                    } label: {
-                                        Text(quiz[stage].answers[2]).frame(width:300)
-                                    }
-                                    .tag(2)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
-                                    
-                                    Button {
-                                        
-                                        buttonClick(3)
-                                    } label: {
-                                        Text(quiz[stage].answers[3]).frame(width:300)
-                                    }
-                                    .tag(3)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
-                                    
-                                }
-                                .font(.largeTitle)
-                            }
-                            .padding()}}
-                        HStack{
-                            Button { reset()
-                            } label: {
-                                Text("초기화")
-                                    .font(.title)
-                                    .padding(.horizontal, 50.0)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 30).stroke(Color.gray, lineWidth: 2))
-                                
-                            }
-                            
-                            Text("기회x \(count)")
-                                .foregroundColor(.red)
-                                .font(.title)
-                                .padding(.horizontal, 50.0)
-                            //버튼을 원형으로 만드는 디자인
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 30).stroke(Color.gray, lineWidth: 2))
-                        }
-                        .padding(.bottom, 10.0)
-                Text("\(answer) / 10").font(.largeTitle).foregroundColor(.blue)
-                Spacer()
-                
-//                        마지막 문제 후, 화면이동을 위한 NavigationLink. 마지막 disabled의 true/false를 이용해 마지막에만 활성화
-//                                                    NavigationLink(destination: LastPage()) {
-//                                                        Text("Next Level").font(.largeTitle)}
-//                                                    .disabled(onOff)
-
+                        }}} else { Text("이제 처음으로 돌아가세요🫠").font(.title).foregroundColor(.blue)}
+                    Spacer()
                 }
+                
+                
+            } else {
+                Text("Round \(stage+1) 드가자~").font(.largeTitle).foregroundColor(.cyan)
+                
+                // 이미지를 stage(Int)에 맞게 넣어주기
+                Image(quiz[stage].imgName)
+                    .resizable() // 이미지가 화면에 fit하게 만듬
+                    .frame(width: 350, height: 300)
+                    .padding()
+                VStack{
+                    HStack{
+                        VStack{
+                            Text("1번")
+                            Text("2번")
+                            Text("3번")
+                            Text("4번")
+                            
+                        }
+                        .font(.largeTitle)
+                        VStack{
+                            
+                            Button {
+                                buttonClick(0)
+                            } label: {
+                                //보기란을 stage(Int)에 맞게 넣어주기
+                                Text(quiz[stage].answers[0]).frame(width:300)
+                            }
+                            .tag(0)// 정답 answerIndex와 tag를 일치시켜서 정답확인 가능하게 함
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
+                            
+                            Button {
+                                
+                                buttonClick(1)
+                            } label: {
+                                Text(quiz[stage].answers[1]).frame(width:300)
+                            }
+                            .tag(1)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
+                            
+                            Button {
+                                
+                                buttonClick(2)
+                            } label: {
+                                Text(quiz[stage].answers[2]).frame(width:300)
+                            }
+                            .tag(2)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
+                            
+                            Button {
+                                
+                                buttonClick(3)
+                            } label: {
+                                Text(quiz[stage].answers[3]).frame(width:300)
+                            }
+                            .tag(3)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
+                            
+                        }
+                        .font(.largeTitle)
+                    }
+                    .padding()}}
+            HStack{
+                Button { reset()
+                } label: {
+                    Text("초기화")
+                        .font(.title)
+                        .padding(.horizontal, 50.0)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 30).stroke(Color.gray, lineWidth: 2))
+                    
+                }
+                
+                Text("기회x \(count)")
+                    .foregroundColor(.red)
+                    .font(.title)
+                    .padding(.horizontal, 50.0)
+                //버튼을 원형으로 만드는 디자인
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30).stroke(Color.gray, lineWidth: 2))
             }
-        //}
+            .padding(.bottom, 10.0)
+            Text("\(answer) / 10").font(.largeTitle).foregroundColor(.blue)
+            Spacer()
+            
+            //                        마지막 문제 후, 화면이동을 위한 NavigationLink. 마지막 disabled의 true/false를 이용해 마지막에만 활성화
+            //                                                    NavigationLink(destination: LastPage()) {
+            //                                                        Text("Next Level").font(.largeTitle)}
+            //                                                    .disabled(onOff)
+            
+        }
+    }
+    //}
     
- // 점수, 돌아가기 버튼 추가, 다음단계(quiz2)누르기
+    // 점수, 돌아가기 버튼 추가, 다음단계(quiz2)누르기
     
     
     func buttonClick(_ tag:Int) {
@@ -163,25 +168,17 @@ struct Practice01: View {
         quiz = quizArr1
     }
     
-    func nextLevel(){
+    func nextLevel(){ //다음단계를 위한 기능
         
         quiz = quizArr2
         stage = 0
         onOff = true
+        onOff2 = true
         answer = 0
     }
     
     
 }
-//struct DetailView: View {
-//    @Binding var showDetail: Bool
-//
-//    var body: some View {
-//        VStack {
-//            Text("This is the Detail View")
-//        }
-//    }
-//}
 
 
 struct Practice01_Previews: PreviewProvider {
